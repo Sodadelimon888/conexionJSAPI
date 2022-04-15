0c4c9fa6c1765a5eb0cc1e5acd26c95ce3593a79
Sodadelimon888 <kharoldsamir@gmail.com>
2022-04-15 00:02:53 -0500

    conexion con la API exitosa

--- js/validate.js
+++ js/validate.js
@@ -6,7 +6,7 @@ const botonEnviar = document.querySelector(".btn-enviar");
 const nameContact = document.getElementsByName("name_contact")[0];
 const email = document.getElementsByName("email_contact")[0];
 const phone = document.getElementsByName("phone_contact")[0];
-const topic = document.getElementById("topic_contact");
+const topic = document.getElementById("topic-contact");
 const commit = document.getElementsByName("commit_contact")[0];
 
 //Mensajes de error
@@ -14,17 +14,38 @@ const errorsList = document.getElementById("errors");
 
 function showError(element, message) {
     element.classList.toggle("error");
-    errorsList.innerHTML += `<li>${message}</li>`;
+    alert(message);
+    // errorsList.innerHTML += `<li>${message}</li>`;
 }
 
-function cleanErrors() {
-    errorsList.innerHTML = '';
-}
+// function cleanErrors() {
+//     errorsList.innerHTML = '';
+// }
+
+/* 5. Enviar petición a la API entregada en clase 
+    (https://30kd6edtfc.execute-api.us-east-1.amazonaws.com/prod/send-email) 
+    usando fetch*/
+    async function sendEmail(name, email, phone, select, comment){
+        const rawResponse = await fetch('https://30kd6edtfc.execute-api.us-east-1.amazonaws.com/prod/send-email', {
+            method: 'POST',
+            headers: {
+                'Accept': 'aplication/json',
+                'Content-type': 'aplication/json'
+            },
+            body: JSON.stringify({name, email, phone, select, comment})  
+        });
+        const content = await rawResponse.json();
+        if (Object.keys(content.errors).length < 0) {
+            alert("Error a enviar los datos del formulario");
+        }else{
+            alert("Correo enviado satisfactoriamente");
+        }
+    }
 
 //Validacion de los elementos
 botonEnviar.addEventListener("click", (event) => {
     event.preventDefault();
-    cleanErrors();
+    // cleanErrors();
     let hasErrors = false;
     
     //1. Campo nombre y apellido no debe estar vacío y contener al menos un espacio.
@@ -57,5 +78,9 @@ botonEnviar.addEventListener("click", (event) => {
         hasErrors = true;
     }
 
-    // TODO: Enviar consulta a API en caso de que el formulario esté correcto
+    //Enviando a la API
+    if (!hasErrors) {
+        sendEmail(sanitizedName, email.value ,sanitizedPhone,topic.value, sanitizedCommit);
+    }
+
 });
